063f96577ae48fb08766ee1a0d212b7a84d3c46a
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-03 11:56:58 -0700

    added Animal, Dog, and Dragon classes

--- animals.py
+++ animals.py
@@ -1,5 +1,5 @@
 class Animal(object):
-    def __init__(self, name, health):
+    def __init__(self, name, health=100):
         self.name = name
         self.health = health
 
@@ -19,9 +19,26 @@ class Animal(object):
         return self
 
     def display_health(self):
-        print "Health:", self.health
+        print "{}'s health: {}".format(self.name, self.health)
 
 
 class Dog(Animal):
-    super(Dog, self).__init__()
-    
+    def __init__(self, name, health=150):
+        super(Dog, self).__init__(name, health)
+
+    def pet(self):
+        self.health += 5
+        return self
+
+
+class Dragon(Animal):
+    def __init__(self, name, health=170):
+        super(Dragon, self).__init__(name, health)
+
+    def fly(self):
+        self.health -= 10
+        return self
+
+    def display_health(self):
+        super(Dragon, self).display_health()
+        print 'I am a Dragon'
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from animals import Animal, Dog
+from animals import Animal, Dog, Dragon
 
-animal1 = Animal('chicken',100)
+animal1 = Animal('Generic Animal',100)
 animal1.walk().walk().walk().run().run().display_health()
 
+dog1 = Dog('Spot')
+dog1.walk().walk().walk().run().run().pet().display_health()
+
+dragon1 = Dragon('Trogdor')
+dragon1.walk().walk().fly().display_health()
+
+animal2 = Animal('New Animal')
+# animal2.pet()
+# animal2.fly()
+
+dog2 = Dog('Bruno')
+# dog2.fly()
