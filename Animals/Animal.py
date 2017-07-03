dfe81c9fc2cc223cec563233d6ac473c2292145a
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-03 11:19:59 -0700

    initial commit

---
+++ Animals/Animal.py
@@ -0,0 +1,23 @@
+class Animal(object):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def walk(self):
+        self.health -= 1
+        self.update_health()
+        return self
+    
+    def run(self):
+        self.health -= 5
+        self.update_health()
+        return self
+
+    def update_health(self):
+        if self.health < 0:
+            self.health = 0
+        return self
+
+    def display_health(self):
+        print "Health:", self.health
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from Animals.Animal import Animal
+
+animal1 = Animal('chicken',100)
+animal1.walk().walk().walk().run().run().display_health()
